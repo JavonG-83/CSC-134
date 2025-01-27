c4fd4648e9967fe21667e51ad1a26db649ef43a5
JavonG-83 <greenj8783@student.faytechcc.edu>
2025-01-27 18:38:26 +0000

    Finished M1T1

---
+++ ASection/M1T1Greenj.cpp
@@ -0,0 +1,12 @@
+ // CSC-134
+ // Note: Today, we start off by typing "hello world" again; wish myself luck
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    cout << endl;
+    return 0;
+    
+}
